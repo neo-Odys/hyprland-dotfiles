a48c9106ffa5ed4fc9d427a995df9fe26f9754e5
Odys <neo-Odys@users.noreply.github.com>
2025-07-02 11:09:36 +0200

    hypr,waybar,wofi

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+HYPR_SOURCE="$HOME/.config/hypr"
+WAYBAR_SOURCE="$HOME/.config/waybar"
+WOFI_SOURCE="$HOME/.config/wofi"
+
+HYPR_DEST="$(pwd)/.config"
+WAYBAR_DEST="$(pwd)/.config"
+WOFI_DEST="$(pwd)/.config"
+
+cp -r "$HYPR_SOURCE" "$HYPR_DEST"
+cp -r "$WAYBAR_SOURCE" "$WAYBAR_DEST"
+cp -r "$WOFI_SOURCE" "$WOFI_DEST"
